e57b442641ca8a6982743915fff30646f6b1487b
Marius Lefter <work@company.com>
2025-06-03 17:39:49 +0200

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import { ppEditorialNewUltralightItalic, inter } from "../lib/fonts"
 import type React from "react"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Marius Lefter | Portfolio",
@@ -23,6 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
