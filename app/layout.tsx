0b12b44d8c9138c13ef5cfac2339fd6d6bd12ad1
Marius Lefter <work@company.com>
2025-06-03 18:07:42 +0200

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,23 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Marius Lefter | Portfolio",
-  description: "Software Engineer"
+  description: "Software engineer focused on full stack web apps and Artificial Intelligence.",
+  openGraph: {
+    title: "Marius Lefter | Portfolio",
+    description: "Software engineer focused on full stack web apps and Artificial Intelligence.",
+    url: "https://www.mariuslefter.com", // your site URL
+    siteName: "Marius Lefter Portfolio",
+    images: [
+      {
+        url: "https://www.mariuslefter.com/og-image.png", // your image URL
+        width: 1200,
+        height: 630,
+        alt: "Marius Lefter Portfolio"
+      }
+    ],
+    locale: "en_US",
+    type: "website"
+  }
 }
 
 export default function RootLayout({
